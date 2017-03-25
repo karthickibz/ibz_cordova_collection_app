d803ce1741828a5225659784280d61255d34f1b5
psaravanan <psaravanan11@gmail.com>
2017-03-25 12:55:17 +0530

    first commit

---
+++ platforms/android/assets/www/js/index.js
@@ -0,0 +1,11 @@
+var app = {
+  initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
+
+  onDeviceReady: function() {
+    // this.receivedEvent('deviceready');
+  }
+};
+
+app.initialize();
